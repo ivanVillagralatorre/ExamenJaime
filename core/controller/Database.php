8dda6de9eb5dfe01daf5c27d29a22102b6c8b3be
iván.villagra <56157806+ivanVillagralatorre@users.noreply.github.com>
2021-02-04 08:34:45 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,13 +3,13 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="b7d5b3ba299cdf";$this->pass="65a96e3b";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_a58c4945a29ac51";
 		//Copiar también la información de la conexión en la función de abajo connect()
 	}
 
 	function connect(){
 		//Copiar aquí también la info de la conexión
-		$this->user="b8ba9d94f28aff";$this->pass="a8337514";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_f51a3950727cd14";
+		$this->user="b7d5b3ba299cdf";$this->pass="65a96e3b";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_a58c4945a29ac51";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
